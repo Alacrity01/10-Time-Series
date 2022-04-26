fc79358a127020ca3b6ac30a74955e0db4b22337
Jeff Brinker <43593600+Alacrity01@users.noreply.github.com>
2022-04-25 22:22:42 -0500

    Monday Apr. 25 code state

---
+++ custom_methods.py
@@ -0,0 +1,10 @@
+class custom_methods:
+	def __init__(self):
+		pass
+
+	def view_first_and_last_five(df):
+		display(df.head(5))
+		display(df.tail(5))
+		
+	def test():
+		return "Success! Loaded custom_methods"
